092fba239fd8ab81bee5ad57b05a73b8c9bd3e79
Jennie <jennie.childers01@gmail.com>
2023-01-06 20:28:59 -0700

    changed code to reflect Bond quote

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,13 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        //inputs first and last names
+        Console.WriteLine("What's your first name? ");
+        string firstName = Console.ReadLine();
+        Console.WriteLine("What's your last name? ");
+        string lastName = Console.ReadLine();
+
+        //outputs modified James Bond quote.
+        Console.WriteLine($"The name's {lastName}, {firstName} {lastName}.");
     }
 }
