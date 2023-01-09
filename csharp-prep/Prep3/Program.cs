811a514e623a5ca2dacc3eaa725dd79ac2903c62
Jennie <jennie.childers01@gmail.com>
2023-01-09 16:04:16 -0700

    random number guessing game

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,27 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+
+        Random randomGenerator = new Random();
+        int number = randomGenerator.Next(1, 101);
+        int answer = 0;
+        int turns = 0;
+
+        while(answer != number){
+            Console.Write("Guess the magic number: ");
+            answer = int.Parse(Console.ReadLine());
+
+            turns += 1;
+            if(answer > number){
+                Console.WriteLine("Lower");
+            }else if(answer < number){
+                Console.WriteLine("Higher");
+            }else{
+                Console.WriteLine("You guessed it!");
+            }
+        }
+
+        Console.WriteLine($"You got it in {turns} turns.");
+
     }
 }
